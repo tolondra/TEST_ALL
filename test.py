d3b14fc9435502316ad08cda0a15c470f2daf96a
Natalia Bedoya <OlgaNatalia.Bedoya-Martinez@mcl.at>
2023-03-09 16:36:25 +0100

    new file:   test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 27 13:58:23 2022
+
+Python stupid tool to test licencese and doc
+
+@author: O.Bedoya
+"""
+import pandas as pd
+
+
+# Need to merge certain option
+def ListOptions():
+    ''' List the possible actions of this script '''
+    return()
+
+def ModSoftData():  
+      '''Modify Software data (Option 4)'''
+      return()
+
+  print("JUST testing")
